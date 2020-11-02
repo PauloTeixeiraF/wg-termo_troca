a1b24761422690a4830ac5df46becfadf2b664f6
Paulo Teixeira <paulo.teixeira.f@outlook.com>
2020-11-02 20:53:34 -0300

    estrutura inicial

---
+++ src/main/java/br/com/hibridus/termoretirada/TermoRetiradaApplication.java
@@ -0,0 +1,13 @@
+package br.com.hibridus.termoretirada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermoRetiradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermoRetiradaApplication.class, args);
+	}
+
+}
